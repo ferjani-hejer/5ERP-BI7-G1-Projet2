e81bdd36032d15b6a6dc79307af58844e66ed1cc
SarraBM84 <sarra.benmessaoud@esprit.tn>
2023-11-12 23:08:44 +0100

    Test unitaire retrieve All Universities

---
+++ src/test/java/tn/esprit/spring/kaddem/UniversiteServiceImplTest.java
@@ -0,0 +1,44 @@
+package tn.esprit.spring.kaddem;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import tn.esprit.spring.kaddem.entities.Departement;
+import tn.esprit.spring.kaddem.entities.Etudiant;
+import tn.esprit.spring.kaddem.entities.Universite;
+import tn.esprit.spring.kaddem.services.DepartementServiceImpl;
+import tn.esprit.spring.kaddem.services.EtudiantServiceImpl;
+import tn.esprit.spring.kaddem.services.UniversiteServiceImpl;
+
+import java.util.List;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@Slf4j
+public class UniversiteServiceImplTest {
+    @Autowired
+    private UniversiteServiceImpl universiteService;
+
+    @Test
+    public void testretrieveAllUniversites() {
+
+        Universite univ = new Universite();
+        univ.setIdUniv(1);
+        univ.setNomUniv("ESPRIT");
+
+        Universite newUniv1 = universiteService.addUniversite(univ);
+        List<Universite> universites = universiteService.retrieveAllUniversites();
+        assertNotNull(universites);
+        assertEquals(1, universites.size());
+        universiteService.deleteUniversite(newUniv1.getIdUniv());
+
+    }
+
+}
